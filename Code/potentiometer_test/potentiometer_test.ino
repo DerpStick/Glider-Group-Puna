270ae3ffcffade819c735f37885e1a511c28f15e
Hamza23476 <132409631+Hamza23476@users.noreply.github.com>
2023-05-31 10:53:34 +1200

    Update potentiometer_test.ino

--- Code/potentiometer_test/potentiometer_test.ino
+++ Code/potentiometer_test/potentiometer_test.ino
@@ -1,9 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
 
+// Constants for servo and potentiometer pins
+const int servoPin = 9;           // PWM pin connected to servo motor
+const int potentiometerPin = A0;  // Analog input pin connected to potentiometer
+
+Servo servoMotor;  // Create a servo object
+
+void setup() {
+  servoMotor.attach(servoPin);  // Attaches the servo to the specified pin
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  int potValue = analogRead(potentiometerPin);  // Read the potentiometer value
+  int servoAngle = map(potValue, 0, 1023, 0, 180);  // Map the potentiometer value to servo angle range
 
+  Serial.println(potValue);
+
+  servoMotor.write(servoAngle);  // Set the servo motor position based on the mapped angle
+
+  delay(15);  // Delay for smoother control (adjust as needed)
 }
